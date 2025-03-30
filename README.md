1543de838b78defaa86678222c563991b989f539
Bavnit Singh <chhabdabavnitsingh@gmail.com>
2025-03-30 11:37:52 +0530

    Setup the Store

--- README.md
+++ README.md
@@ -4,4 +4,15 @@
 - Remove unnecessary code and create a hello world app
 - Install Tailwind css and daisy Ui plugin
 - Add navbar Component to it
+- Create Outlet and Footer
+- Create a Login page
+- Install Axios
+- CORS - install cors package in backend , use it as a middleware by app.use(cors()) where in cors you have to give the origin and credetials : true 
+- Whenever you are making Api call pass {withCredentials:true} for cookie in frontend
+- install react-redux and @redux/toolkit : https://redux-toolkit.js.org/tutorials/quick-start
+- configure Store =><Provider store={appStore}> to  your app.jsx => create a slice => add reducer to store
 
+- Add Redux Devtools in Brave/Chrome
+- Login and see if your data is coming properly or not into the store
+- Navbar should update as soon as User logs in 
+- Refactor our code , Create constants file and add BASE_URL there and make Components folder and all the components into it
---
+++ src/Components/Login.jsx
@@ -0,0 +1,75 @@
+import axios from "axios";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
+
+const Login = () => {
+  const [emailId, setEmailId] = useState("bavnit@gmail.com");
+  const [password, setPassword] = useState("Bavnit@1234");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/login",
+        {
+          emailId,
+          password,
+        },
+        { withCredentials: true }
+      );
+      //  console.log(res.data);
+      dispatch(addUser(res.data));
+      return navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return (
+    <div className="flex justify-center" style={{ margin: "100px" }}>
+      <div className="card bg-base-200 w-96 shadow-sm ">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+          <label htmlFor="Email">Email : </label>
+          <input
+            type="text"
+            placeholder=" Enter your Email"
+            value={emailId}
+            className="justify-center"
+            onChange={(e) => setEmailId(e.target.value)}
+            style={{
+              padding: "10px",
+              margin: "5px",
+              height: "20px",
+              borderRadius: "5px",
+            }}
+          />
+
+          <label htmlFor="Password">Password : </label>
+          <input
+            type="text"
+            placeholder="Enter your Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="justify-center"
+            style={{
+              padding: "10px",
+              margin: "5px",
+              height: "20px",
+              borderRadius: "5px",
+            }}
+          />
+          <div className="card-actions justify-center">
+            <button className="btn btn-info" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
