bc25fe756228c6605badc41e5b34f9ef21580961
Bavnit Singh <chhabdabavnitsingh@gmail.com>
2025-04-10 13:12:27 +0530

    handled "/" navigation on login condition

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Body from "./Components/Body";
 import Login from "./Components/Login";
 import Profile from "./Components/Profile";
-import { Provider } from "react-redux";
+import { Provider, useSelector } from "react-redux";
 import appStore from "./utils/appStore";
 import Feed from "./Components/Feed";
 import Connections from "./Components/Connections"
 import Request from "./Components/Request";
 
 function App() {
+  const user = useSelector((store) => store.user); 
   return (
     <>
       <Provider store={appStore}>
         <BrowserRouter basename="/">
           <Routes>
             <Route path="/" element={<Body />}>
-              <Route path="/" element={<Feed />} />
+              <Route
+                index
+                element={user ? <Feed /> : <Navigate to="/login" replace />}
+              />
 
               <Route path="/login" element={<Login />} />
               <Route path="/profile" element={<Profile />} />
