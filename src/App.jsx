c5aec2d5438f7dc52e6597e1ac3dec8812b8948e
Bavnit Singh <chhabdabavnitsingh@gmail.com>
2025-04-10 13:15:17 +0530

    Undo Changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,21 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Body from "./Components/Body";
 import Login from "./Components/Login";
 import Profile from "./Components/Profile";
-import { Provider, useSelector } from "react-redux";
+import { Provider } from "react-redux";
 import appStore from "./utils/appStore";
 import Feed from "./Components/Feed";
 import Connections from "./Components/Connections"
 import Request from "./Components/Request";
 
 function App() {
-  const user = useSelector((store) => store.user); 
   return (
     <>
       <Provider store={appStore}>
         <BrowserRouter basename="/">
           <Routes>
             <Route path="/" element={<Body />}>
-              <Route
-                index
-                element={user ? <Feed /> : <Navigate to="/login" replace />}
-              />
+              <Route path="/" element={<Feed />} />
 
               <Route path="/login" element={<Login />} />
               <Route path="/profile" element={<Profile />} />
